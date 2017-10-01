38f15c972e9489279e344db29ac31ccd4c83beb7
Heda Wang <whd.thu@gmail.com>
2017-10-02 01:17:04 +0800

    add train.sh

---
+++ train.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# the script directory
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+INCEPTION_CHECKPOINT="${DIR}/pretrained_model/inception_v3/inception_v3.ckpt"
+TFRECORD_DIR="${DIR}/data/TFRecord_data"
+MODEL_DIR="${DIR}/model"
+
+model_dir_name=show_and_tell_model
+
+cd im2txt && CUDA_VISIBLE_DEVICES=0 python train.py \
+  --input_file_pattern="${TFRECORD_DIR}/train-?????-of-?????.tfrecord" \
+  --inception_checkpoint_file="${INCEPTION_CHECKPOINT}" \
+  --train_dir="${MODEL_DIR}/${model_dir_name}" \
+  --number_of_steps=1000000
