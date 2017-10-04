c658d09a24e5017be8353035401ec6f40b9972f7
Heda Wang <whd.thu@gmail.com>
2017-10-04 14:29:21 +0800

    add eval.sh (tested)

--- eval.sh
+++ eval.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 model_name="show_and_tell_model"
 num_processes=3
 gpu_fraction=0.28
 device=1
 ckpt=101463
 
-for ckpt in 65271 129123 193009 257007 322481; do 
+for ckpt in 65271 129123 193009 257007 322481 387562; do 
   # the script directory
-  DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
   MODEL_DIR="${DIR}/model/${model_name}"
-  IMAGE_DIR="${DIR}/data/ai_challenger_caption_test1_20170923/caption_test1_images_20170923"
+  VALIDATE_IMAGE_DIR="${DIR}/data/ai_challenger_caption_validation_20170910/caption_validation_images_20170910"
   VALIDATE_REFERENCE_FILE="${DIR}/data/ai_challenger_caption_validation_20170910/reference.json"
 
   CHECKPOINT_PATH="${MODEL_DIR}/model.ckpt-$ckpt"
@@ -21,23 +22,19 @@ for ckpt in 65271 129123 193009 257007 322481; do
   cd ${DIR}/im2txt
 
   for prefix in 0 1 2 3 4 5 6 7 8 9 a b c d e f; do 
-    echo "CUDA_VISIBLE_DEVICES=$device python inference.py \
-      --input_file_pattern='${IMAGE_DIR}/${prefix}*.jpg' \
-      --checkpoint_path=${CHECKPOINT_PATH} \
-      --vocab_file=${DIR}/data/word_counts.txt \
-      --output=${OUTPUT_DIR}/part-${prefix}.json \
-      --gpu_memory_fraction=$gpu_fraction"
+    if [ ! -f ${OUTPUT_DIR}/part-${prefix}.json ]; then
+      echo "CUDA_VISIBLE_DEVICES=$device python inference.py \
+        --input_file_pattern='${VALIDATE_IMAGE_DIR}/${prefix}*.jpg' \
+        --checkpoint_path=${CHECKPOINT_PATH} \
+        --vocab_file=${DIR}/data/word_counts.txt \
+        --output=${OUTPUT_DIR}/part-${prefix}.json \
+        --gpu_memory_fraction=$gpu_fraction"
+    fi
   done | parallel -j $num_processes
 
-  cd ${DIR}
-
-  python tools/merge_json_lists.py ${OUTPUT_DIR}/part-?.json > ${OUTPUT_DIR}/out.json
-
+  python ${DIR}/tools/merge_json_lists.py ${OUTPUT_DIR}/part-?.json > ${OUTPUT_DIR}/out.json
   echo output saved to ${OUTPUT_DIR}/out.json
 
-  cd ${DIR}/tools/eval
-
-  python run_evaluations.py --submit ${OUTPUT_DIR}/out.json --ref $VALIDATE_REFERENCE_FILE | tee ${OUTPUT_DIR}/out.eval | grep ^Eval
-
+  python ${DIR}/tools/eval/run_evaluations.py --submit ${OUTPUT_DIR}/out.json --ref $VALIDATE_REFERENCE_FILE | tee ${OUTPUT_DIR}/out.eval | grep ^Eval
   echo eval result saved to ${OUTPUT_DIR}/out.eval
 done
