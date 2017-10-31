d574a5fcff01f7eaf18d8b3cf63f0f7c46755322
Heda Wang <whd.thu@gmail.com>
2017-10-31 10:34:54 +0100

    add hyper parameter scripts

---
+++ advanced-visual-attention-2lexical-eval-2.0-0.6-8.0.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+model_name="show_and_tell_advanced_model_visual_attention_2lexical-2.0-0.6-8.0"
+num_processes=1
+gpu_fraction=0.97
+device=0
+model=ShowAndTellAdvancedModel
+
+MODEL_DIR="${DIR}/model/${model_name}"
+for ckpt in $(ls ${MODEL_DIR} | python ${DIR}/tools/every_n_step.py 20000); do 
+  # the script directory
+  VALIDATE_IMAGE_DIR="${DIR}/data/ai_challenger_caption_validation_20170910/caption_validation_images_20170910"
+  VALIDATE_REFERENCE_FILE="${DIR}/data/ai_challenger_caption_validation_20170910/reference.json"
+
+  CHECKPOINT_PATH="${MODEL_DIR}/model.ckpt-$ckpt"
+  OUTPUT_DIR="${MODEL_DIR}/model.ckpt-${ckpt}.eval"
+
+  mkdir $OUTPUT_DIR
+
+  cd ${DIR}/im2txt
+
+  for prefix in 0 1 2 3 4 5 6 7 8 9 a b c d e f; do 
+    if [ ! -f ${OUTPUT_DIR}/part-${prefix}.json ]; then
+      echo "CUDA_VISIBLE_DEVICES=$device python inference.py \
+        --input_file_pattern='${VALIDATE_IMAGE_DIR}/${prefix}*.jpg' \
+        --checkpoint_path=${CHECKPOINT_PATH} \
+        --vocab_file=${DIR}/data/word_counts.txt \
+        --output=${OUTPUT_DIR}/part-${prefix}.json \
+        --model=${model} \
+        --inception_return_tuple=True \
+        --use_attention_wrapper=True \
+        --attention_mechanism=BahdanauAttention \
+        --num_lstm_layers=1 \
+        --use_lexical_embedding=True \
+        --lexical_mapping_file='${DIR}/data/word2postag.txt,${DIR}/data/word2char.txt' \
+        --lexical_embedding_type='postag,char' \
+        --lexical_embedding_size='32,128' \
+        --support_ingraph=True"
+    fi
+  done | bash
+
+  if [ ! -f ${OUTPUT_DIR}/out.json ]; then
+    python ${DIR}/tools/merge_json_lists.py ${OUTPUT_DIR}/part-?.json > ${OUTPUT_DIR}/out.json
+    echo output saved to ${OUTPUT_DIR}/out.json
+  fi
+
+  if [ ! -f ${OUTPUT_DIR}/out.eval ]; then
+    python ${DIR}/tools/eval/run_evaluations.py --submit ${OUTPUT_DIR}/out.json --ref $VALIDATE_REFERENCE_FILE | tee ${OUTPUT_DIR}/out.eval | grep ^Eval
+    echo eval result saved to ${OUTPUT_DIR}/out.eval
+  fi
+done
