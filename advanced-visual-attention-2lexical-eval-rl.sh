f7d3481f539e481ac6fcc52716e7e75d2c3bf09e
Heda Wang <whd.thu@gmail.com>
2017-11-29 11:09:11 +0800

    add train and eval rl to visual attention 2lexical

---
+++ advanced-visual-attention-2lexical-eval-rl.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+model_name="show_and_tell_advanced_model_visual_attention_2lexical"
+num_processes=1
+gpu_fraction=0.97
+device=1
+model=ShowAndTellAdvancedModel
+
+MODEL_DIR="${DIR}/model/${model_name}"
+for ckpt in $(ls ${MODEL_DIR} | python ${DIR}/tools/every_n_step.py 8000 | tail -n 20 | tac); do 
+  # the script directory
+  VALIDATE_IMAGE_DIR="${DIR}/data/ai_challenger_caption_validation_20170910/caption_validation_images_20170910"
+  VALIDATE_REFERENCE_FILE="${DIR}/data/ai_challenger_caption_validation_20170910/reference.json"
+
+  CHECKPOINT_PATH="${MODEL_DIR}/model.ckpt-$ckpt"
+  OUTPUT_DIR="${MODEL_DIR}/model.ckpt-${ckpt}.eval"
+
+  mkdir $OUTPUT_DIR
+
+  cd ${DIR}/im2txt
+
+  if [ ! -f ${OUTPUT_DIR}/out.json ]; then
+    CUDA_VISIBLE_DEVICES=$device python inference.py \
+      --input_file_pattern="${VALIDATE_IMAGE_DIR}/${prefix}*.jpg" \
+      --checkpoint_path=${CHECKPOINT_PATH} \
+      --vocab_file=${DIR}/data/word_counts.txt \
+      --output=${OUTPUT_DIR}/out.json \
+      --model=${model} \
+      --inception_return_tuple=True \
+      --use_attention_wrapper=True \
+      --attention_mechanism=BahdanauAttention \
+      --num_lstm_layers=1 \
+      --use_lexical_embedding=True \
+      --lexical_mapping_file="${DIR}/data/word2postag.txt,${DIR}/data/word2char.txt" \
+      --lexical_embedding_type='postag,char' \
+      --lexical_embedding_size='32,128' \
+      --support_ingraph=True
+    echo output saved to ${OUTPUT_DIR}/out.json
+  fi
+
+  if [ ! -f ${OUTPUT_DIR}/out.eval ]; then
+    python ${DIR}/tools/eval/run_evaluations.py --submit ${OUTPUT_DIR}/out.json --ref $VALIDATE_REFERENCE_FILE | tee ${OUTPUT_DIR}/out.eval | grep ^Eval
+    echo eval result saved to ${OUTPUT_DIR}/out.eval
+  fi
+done
